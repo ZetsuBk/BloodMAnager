94bc249c759a9b9eba7803c33000f243e15d8042
ZetsuBk <blackzetsu.no@gmail.com>
2024-05-01 15:59:42 +0100

    create spring application

---
+++ BackEnd/src/main/java/com/example/bloodmanager/BloodmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloodmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodmanagerApplication.class, args);
+	}
+
+}
